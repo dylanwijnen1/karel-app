080f54174bed4a8f54cbcf9564df5063760e78dc
Dylan Wijnen <dylan.wijnen1@gmail.com>
2020-10-09 17:45:04 -0700

    Updates

---
+++ precache-manifest.93f3181d3c8d9eee7b4f947fff47eca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c4e3306e5bf69ea2b2b100db0da096",
+    "url": "/karel-app/index.html"
+  },
+  {
+    "revision": "0e8d4ecf344af6593c32",
+    "url": "/karel-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c73525807d6d7a925d9",
+    "url": "/karel-app/static/js/2.611bfbed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karel-app/static/js/2.611bfbed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8d4ecf344af6593c32",
+    "url": "/karel-app/static/js/main.b58aff78.chunk.js"
+  },
+  {
+    "revision": "8d332f0a71ec68923126",
+    "url": "/karel-app/static/js/runtime-main.1b2ae9b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/karel-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
